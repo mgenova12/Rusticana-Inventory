c478da40e41554fba819780c098cd70831abca95
Marco <mgenova03@gmail.com>
2018-11-16 12:41:30 -0500

    add price change indicators to price overview

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -2,7 +2,7 @@ class PricesController < ApplicationController
   def index
     # last_record_date = Price.maximum(:created_at)
     # @prices = Price.where(:created_at => (last_record_date.at_beginning_of_day)..(last_record_date.end_of_day))
-    @saved_price_dates = SavedPrice.all.reverse_order
+    @saved_price_dates = SavedPrice.all
     @products = Product.order(:name)
   end
 
@@ -12,8 +12,6 @@ class PricesController < ApplicationController
 
   def show 
     @prices = Price.where(saved_price_id: params[:saved_price_id])
-    @previous_prices = Price.where(saved_price_id: (params[:saved_price_id].to_i - 1))
-
     @date = SavedPrice.find(params[:saved_price_id]).created_at
   end
 
