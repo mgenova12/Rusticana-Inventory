8a03ea46165fe464cdf9bd26253de8e564a55615
Marco <mgenova03@gmail.com>
2019-01-01 11:41:45 -0500

    fix datatype inventory bug

--- app/helpers/inventories_helper.rb
+++ app/helpers/inventories_helper.rb
@@ -6,7 +6,7 @@ module InventoriesHelper
       case_result = (case_amount.to_f / inventory[:case_quantity].to_f).ceil
       case_result > 0 ? case_result : 0
     elsif inventory[:measurement] == '%'
-      percent = (inventory[:quantity] * 0.01)
+      percent = (inventory[:quantity].to_i * 0.01)
       percent <= 0.25 ? 1 : 0
     else 
       result = inventory[:max_amount].to_i - inventory[:quantity].to_i
